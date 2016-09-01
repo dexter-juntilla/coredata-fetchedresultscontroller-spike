d51f18735a29f191d809807a8723a6970f8d8e87
Dexter Juntilla <dexter@dnamicro.com>
2016-09-01 14:44:18 +0800

    update and delete records

--- CoreDataListenerSpike/ViewController.h
+++ CoreDataListenerSpike/ViewController.h
@@ -12,12 +12,13 @@
 @interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate>
 
 @property UITableView *tableView;
-
+@property NSManagedObject *selectedRecord;
 @property NSFetchedResultsController *fetchedResultsController;
 
 - (void)getDataFromDB;
 - (void)rightBarButtonPressed;
 - (void)saveItemPressed:(NSString *)title;
+- (void)updateItemPressed:(NSString *)title didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
 
 @end
 
--- CoreDataListenerSpike/ViewController.m
+++ CoreDataListenerSpike/ViewController.m
@@ -18,6 +18,7 @@
 
 @synthesize tableView;
 @synthesize fetchedResultsController;
+@synthesize selectedRecord;
 
 #pragma mark -
 #pragma mark View Life Cycle
@@ -81,6 +82,51 @@
     return cell;
 }
 
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSManagedObject *record = [[self fetchedResultsController] objectAtIndexPath:indexPath];
+        [[[CoreData sharedInstance] masterObjectContext] deleteObject:record];
+    }
+}
+
+#pragma mark -
+#pragma mark Table View Delegate Methods
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    self.selectedRecord = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    
+    NSString *title = [[self selectedRecord] valueForKey:@"title"];
+    
+    UIAlertController * alertController = [UIAlertController alertControllerWithTitle: @"Title"
+                                                                              message: nil
+                                                                       preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.placeholder = @"title";
+        textField.textColor = [UIColor blueColor];
+        textField.text = title;
+        textField.clearButtonMode = UITextFieldViewModeWhileEditing;
+        textField.borderStyle = UITextBorderStyleRoundedRect;
+    }];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        
+    }]];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Update" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        NSArray * textfields = alertController.textFields;
+        UITextField * titleField = textfields[0];
+        NSLog(@"%@",titleField.text);
+        [self updateItemPressed:titleField.text didSelectRowAtIndexPath:indexPath];
+    }]];
+    
+    [self presentViewController:alertController animated:YES completion:^{}];
+}
+
 #pragma mark -
 #pragma mark Fetch Results Controller Delegate methods
 
@@ -131,24 +177,9 @@
         NSLog(@"Unable to perform fetch.");
         NSLog(@"%@, %@", error, error.localizedDescription);
     }
-//    
-//    NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Item" inManagedObjectContext:[[CoreData sharedInstance] masterObjectContext]];
-//    [fetchRequest setEntity:entityDescription];
-//    
-//    NSError *error = nil;
-//    NSArray *result = [[[[CoreData sharedInstance] masterObjectContext] executeRequest:fetchRequest error:&error] finalResult];
-//    
-//    NSMutableArray *temp = [[NSMutableArray alloc] init];
-//    
-//    for (NSManagedObject *obj in result) {
-//        NSArray *keys = [[[obj entity] attributesByName] allKeys];
-//        NSDictionary *dict = [obj dictionaryWithValuesForKeys:keys];
-//        [temp addObject:dict];
-//    }
 }
 
 - (void)rightBarButtonPressed {
-    NSLog(@"%@", @"rightBarButtonPressed");
     UIAlertController * alertController = [UIAlertController alertControllerWithTitle: @"Title"
                                                                               message: nil
                                                                        preferredStyle:UIAlertControllerStyleAlert];
@@ -158,6 +189,11 @@
         textField.clearButtonMode = UITextFieldViewModeWhileEditing;
         textField.borderStyle = UITextBorderStyleRoundedRect;
     }];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        
+    }]];
+    
     [alertController addAction:[UIAlertAction actionWithTitle:@"Save" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
         NSArray * textfields = alertController.textFields;
         UITextField * titleField = textfields[0];
@@ -171,24 +207,21 @@
 - (void)saveItemPressed:(NSString *)title {
     
     if (title && title.length) {
-        // Create Entity
         NSEntityDescription *entity = [NSEntityDescription entityForName:@"Item" inManagedObjectContext:[[CoreData sharedInstance] backgroundObjectContext]];
         
-        // Initialize Record
         NSManagedObject *record = [[NSManagedObject alloc] initWithEntity:entity insertIntoManagedObjectContext:[[CoreData sharedInstance] backgroundObjectContext]];
         
-        // Populate Record
         [record setValue:title forKey:@"title"];
         [record setValue:[NSDate date] forKey:@"createdDate"];
         [record setValue:@NO forKey:@"done"];
-        
-        // Save Record
-        NSError *error = nil;
-        [[CoreData sharedInstance] saveContext];
-        if (error) {
-            NSLog(@"%@", error);
-        }
+        // Save Record on background or terminate
+
     }
+}
+
+- (void)updateItemPressed:(NSString *)title didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [[self selectedRecord] setValue:title forKey:@"title"];
+    // Save Record on background or terminate
 }
 
 @end
